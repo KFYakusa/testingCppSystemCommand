e710f565ebcc6a576f13d6c65624243ab24df289
KFYakusa <daniel_fuzion@hotmail.com>
2022-05-30 09:35:58 -0300

    wip - testing capabilities of system input

--- teste.cpp
+++ teste.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
 	cout<< "file to test simple command input and output."<<endl<<endl;
 	
-	system("git init && echo 'project to test system command input inside cpp files' > README.md; git remote add origin git@github.com:KFYakusa/testingCppSystemCommand.git; git add teste.cpp ; git commit -m 'wip - testing capabilities of system input'; git push origin main;");
+	system("git init && echo 'project to test system command input inside cpp files' > README.md; git remote add origin git@github.com:KFYakusa/testingCppSystemCommand.git; git add teste.cpp ; git commit -m 'wip - testing capabilities of system input'; git push origin master;");
 
 	return EXIT_SUCCESS;
 }
